27f59b6d83b1d5a5f4f77419447e377c27d225bc
henner espinoza <hennerespinoza@henners-Air.attlocal.net>
2023-03-07 23:27:08 -0500

    password variable prompts and reqs

--- Develop/script.js
+++ Develop/script.js
@@ -11,8 +11,23 @@ function generatePassword(){
   var upperCase = 'ABCDEFGHIJKLMNOPQRSTUVWXYZ';
   var lowerCase = 'abcdefghijklmnopqrstuvwxyz';
   var numbers = '1234567890';
-  var symbols = '!\"#$%&'()*+,-./:;<=>?@[\]^_`{|}~"';
+  var specialChars = "!"#$%&'()*+,-./:;<=>?@[\]^_`{|}~";
 
+  // Password container
+  var passwordContainer = "";
+
+  // prompts to confirm variables
+  var passwordLength = prompt("What is the length of the password? Note: length of at least 8 characters and no more than 128 characters");
+  var lowerCaseYN = confirm("Click ok to confirm whether or not to include lowercase letters.");
+  var upperCaseYN = confirm("Click ok to confirm whether or not to include uppercase letters.");
+  var numbericYN = confirm("Click ok to confirm whether or not to include numeric characters.");
+  var specialChars = confirm("Click ok to confirm whether or not to include special characters.");
+
+  // confirm password length meets required specs
+  if (passwordLength < 8 || passwordLength > 128){
+    alert("Eek! Password must be between 8 and 128 characters.");
+    return
+  }
 }
 // Write password to the #password input
 function writePassword() {

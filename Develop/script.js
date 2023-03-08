e8db331f9a8ffde0fb417651900e80429645a917
henner espinoza <hennerespinoza@henners-Air.attlocal.net>
2023-03-08 00:27:08 -0500

    concat variables, create loop based on pw length, set math.floor and math.random
    git add .
    git push origin main

--- Develop/script.js
+++ Develop/script.js
@@ -21,13 +21,48 @@ function generatePassword(){
   var lowerCaseYN = confirm("Click ok to confirm whether or not to include lowercase letters.");
   var upperCaseYN = confirm("Click ok to confirm whether or not to include uppercase letters.");
   var numbericYN = confirm("Click ok to confirm whether or not to include numeric characters.");
-  var specialChars = confirm("Click ok to confirm whether or not to include special characters.");
+  var specialCharsYN = confirm("Click ok to confirm whether or not to include special characters.");
 
   // confirm password length meets required specs
   if (passwordLength < 8 || passwordLength > 128){
     alert("Eek! Password must be between 8 and 128 characters.");
     return
   }
+
+  console.log(passwordLength);
+
+  // if variables are confirmed Y add them to the pw container
+  if (lowerCaseYN){
+    passwordContainer = passwordContainer.concat(lowerCase);
+  }
+  if (upperCaseYN){
+    passwordContainer = passwordContainer.concat(upperCase);
+  }
+  if (numbericYN){
+    passwordContainer = passwordContainer.concat(numbers);
+  }
+  if (specialCharsYN){
+    passwordContainer = passwordContainer.concat(numbers);
+  }
+
+  // alert if user does not select a variable 
+  if (passwordContainer.length === 0) {
+    alert("ALERT: Atleast one character type should be selected.");
+  }
+
+  // loop starting at 0 and incrementing by 1 till reaching selected password length
+  for (var i = 0; i <= passwordLength; i++){
+    var pass;
+
+    // generate rand number that is equal to or less than passwordContainer length
+    var randomNumber = Math.floor(Math.random() * passwordContainer.length + 1);
+
+    // iterate through passwordContainer and pull char character at index of # provided by randomNumber
+    pass += passwordContainer.charAt(randomNumber)
+    console.log(pass);
+  } 
+  // exit function
+  return pass;
 }
 // Write password to the #password input
 function writePassword() {
